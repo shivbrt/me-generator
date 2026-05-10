4534fc7df8aab37350a6e13d2e70f23112937986
Shiv Bartaula <74212245+shivbrt@users.noreply.github.com>
2026-05-10 14:30:37 +1000

    Update site-header.tsx

--- src/app/client/site-header.tsx
+++ src/app/client/site-header.tsx
@@ -7,7 +7,7 @@ export function SiteHeader() {
     const firstName = config.name.split(" ")[0];
 
     return (
-        <header className='sticky top-0 z-50 w-1/2 mx-auto border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
+        <header className='sticky top-0 z-50 w-full md:w-1/2 mx-auto border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
             <div className='container flex h-14 max-w-screen-2xl items-center justify-between px-4'>
                 <Link href='/' className='flex items-center space-x-2'>
                     <span className='font-bold text-lg text-primary'>{firstName}</span>
